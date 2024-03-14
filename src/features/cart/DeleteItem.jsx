a3fa2523b105bf607e44c9578676e43185cd9cde
Denis <paskin.denis.00@gmail.com>
2024-03-14 14:36:09 +0100

    pizza-co project

---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,20 @@
+import PtopTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { deleteItem } from './cartSlice';
+
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
+
+  return (
+    <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+      Delete
+    </Button>
+  );
+}
+
+DeleteItem.propTypes = {
+  pizzaId: PtopTypes.number.isRequired,
+};
+
+export default DeleteItem;
---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { useFetcher } from 'react-router-dom';
+import Button from '../../ui/Button';
+import { updateOrder } from '../../services/apiRestaurant';
+
+function UpdateOrder({ order }) {
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make priority</Button>
+    </fetcher.Form>
+  );
+}
+
+UpdateOrder.propTypes = {
+  order: PropTypes.object,
+};
+
+export default UpdateOrder;
+
+export async function action({ request, params }) {
+  const data = { priority: true };
+  await updateOrder(params.orderId, data);
+  return null;
+}
